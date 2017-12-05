1c489b3d18a3878a3f8f6b09dc6884885a4a0f83
Bhavya Teja <bhavyateja7@gmail.com>
2017-12-04 19:46:26 -0600

    Rolling dice Simulator updated.

---
+++ RollingDiceSimulator.py
@@ -0,0 +1,25 @@
+import random
+
+def dice():
+    b = random.randint(1,6)
+    print('\nThe number the dice produced is', b)
+
+print('You want the Dice to be rolled? ')
+choice = input('\nPlease enter your choice: Yes/No - ')
+if choice == 'Yes' or choice == 'yes':
+    choice = True
+else:
+    choice = False
+
+while(choice):
+        dice()
+        choice = input("Do you want the dice to be rolled again? Enter your choice: Yes/No - ")
+        if choice == 'Yes' or choice == 'yes':
+            choice = True
+        else:
+            choice = False
+
+
+
+
+
