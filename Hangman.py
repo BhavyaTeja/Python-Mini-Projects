61df4d7aebf69aa35544fd0ed35ab20970efe11a
Bhavya Teja <bhavyateja7@gmail.com>
2017-12-06 18:36:54 -0600

    Hangman code updated. Count pending.

--- Hangman.py
+++ Hangman.py
@@ -6,6 +6,8 @@ word_site = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-typ
 response = urllib2.urlopen(word_site)
 txt = response.read()
 WORDS = txt.splitlines()
+guess = []
+count = 0
 
 word = random.choice(WORDS)
 
@@ -15,7 +17,27 @@ def inWord(letter, word):
 def timesInWord(letter, word):
     return word.count('letter')
 
-letter = str(input('Enter a letter'))
+def getLetter():
+    letter = str(input("Enter a letter"))
+    if letter.isalpha() or len(letter) == 1:
+        if inWord(letter, word) and timesInWord(letter, word) >= guess.count('letter'):
+            guess.append('letter')
+            if word[:len(guess)] == ''.join(guess):
+                if len(word) == len(guess):
+                    print('Congratulations! Your guess is correct, the word is ', word)
+                else:
+                    print('Good guess! Guess the next letter')
+                    getLetter()
+            else:
+                print('Wrong guess! Try again')
+                guess.pop()
+                getLetter()
+        else:
+            print('The letter is already present or the letter is not there in the word! Try again')
+            getLetter()
+    else:
+        print('Entered input has a number or it has more characters! Please try again')
+        getLetter()
 
-print(inWord(letter, word))
-
+getLetter()
+print(word)
